31c20fdf39ace8e5a654c63dc952360155cabe36
swanjohi <samuel.wanjohi@tracom.co.ke>
2022-07-17 16:17:01 +0300

    updated StockGeneralResponse generic class

--- src/main/java/com/example/demo/models/StockGeneralResponse.java
+++ src/main/java/com/example/demo/models/StockGeneralResponse.java
@@ -9,4 +9,36 @@ public class StockGeneralResponse<T> {
     private HttpStatus httpStatus;
 
     private UnifiedSet<String> userMessages = UnifiedSet.newSet();
+
+    public StockGeneralResponse() {
+    }
+
+    public StockGeneralResponse(T response, HttpStatus httpStatus) {
+        this.response = response;
+        this.httpStatus = httpStatus;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public UnifiedSet<String> getUserMessages() {
+        return userMessages;
+    }
+
+    public void setUserMessages(UnifiedSet<String> userMessages) {
+        this.userMessages = userMessages;
+    }
 }
